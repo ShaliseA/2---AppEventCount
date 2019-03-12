023c11450df1e3f709f94920a74c038e27071d35
Shalise Ayromloo <shaliseayromloo@10--1-232-170.vlan882.client.wireless.uic.edu>
2019-03-12 16:12:30 -0500

    This is the second app in chapter 4 of App Development With Swift. The objective is to provide visual representation of the app's life cycle by updating the labels on the screen.

--- AppLifeCycle/ViewController.swift
+++ AppLifeCycle/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var didFinishLaunchingLabel: UILabel!
+    
+    @IBOutlet weak var willResignActiveLabel: UILabel!
+    
+    @IBOutlet weak var didEnterBackgroundLabel: UILabel!
+    
+    @IBOutlet weak var willEnterForegroundLabel: UILabel!
+    
+    @IBOutlet weak var didBecomeActiveLabel: UILabel!
+    
+    @IBOutlet weak var willTerminateLabel: UILabel!
+   
+    
+    var didFinishLaunchingCount: Int = 0
+    var willResignActiveCount: Int = 0
+    var didEnterBackgroundCount: Int = 0
+    var willEnterForegroundCount: Int = 0
+    var didBecomeActiveCount: Int = 0
+    var willTerminateCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateView()
+    }
+    
+    
+    func updateView() {
+        didFinishLaunchingLabel.text = "didFinishLaunching has been called  \(didFinishLaunchingCount) times."
+        willResignActiveLabel.text = "willResignActive has been called \(willResignActiveCount) times."
+        didEnterBackgroundLabel.text = "didEnterBackground has been called \(didEnterBackgroundCount) times."
+        willEnterForegroundLabel.text = "willEnterForeground has been called \(willEnterForegroundCount) times."
+        didBecomeActiveLabel.text = "didBecomeActive has been called \(didBecomeActiveCount) times."
+        willTerminateLabel.text = "willTerminate has been called \(willTerminateCount) times."
+        
     }
 
 
